f04b71784e05c1c4096e468dbc78f5fd16be7ffe
Shahad Al_ jawashee <shahad1nazar@gmail.com>
2025-06-16 18:57:37 +0200

    chang the gap for services section

--- src/app/components/ContactInfoSection.js
+++ src/app/components/ContactInfoSection.js
@@ -3,11 +3,11 @@ import { Phone, Mail, Clock, MapPin } from "lucide-react";
 
 export default function ContactInfoCards() {
   return (
-    <section className="bg-darkblue py-16  sm:px-6 lg:px-8">
+    <section className="bg-darkblue py-16 w-full sm:px-6 lg:px-8">
       <div className="max-w-6xl mx-auto">
         <h2 className="text-3xl font-bold text-darkblue mb-12 text-center">Kontakt os</h2>
         
-        <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-10">
           {/* Adresse kort */}
           <div className="bg-white p-6 rounded-xl shadow-lg hover:shadow-accent-md transition-all duration-300 border border-gray-light text-center">
             <div className="flex justify-center mb-4">
