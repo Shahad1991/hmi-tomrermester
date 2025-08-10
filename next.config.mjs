3781f1209f75ef06eba8e4889428da8bce6f8806
Shahad Al_ jawashee <Shahad1991@users.noreply.github.com>
2025-08-10 22:06:29 +0200

    add the domain to next.js config file

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['api.hmi-tomrermester.dk', "hmi-tomrermester.vercel.app"], // Tillad billeder fra underdomænet
+    domains: ['api.hmi-tomrermester.dk', "hmi-tomrermester.vercel.app", "hmi-tomrermester.dk"], // Tillad billeder fra underdomænet
   },
 };
 
