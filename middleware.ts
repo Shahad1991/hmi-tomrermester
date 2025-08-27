a75706c8948a1104ec34e44d476e521e4a151685
Shahad Al_ jawashee <Shahad1991@users.noreply.github.com>
2025-08-27 12:10:14 +0200

    hhh

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,7 @@
 import { NextResponse } from 'next/server'
+import type { NextRequest } from 'next/server'
 
-export function middleware(request) {
+export function middleware(request: NextRequest) {
   const url = request.nextUrl.clone()
   
   // Handle www redirects
